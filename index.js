77068e448c815dffd2b52b4ef5fbd14e0d7f265e
Siddhant Singh <siddhantsingh9023134084@gmail.com>
2022-12-26 13:50:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 let title = document.title;
-let appkey="2y8etr45";
-let itemkey="khu";
-
+let appkey;
+let itemkey;
+function askKey() {
+  appkey = prompt("        Enter Appkey");
+  
+  if (appkey == null || appkey == "") {
+    alert("Error (Appkey) 🚫");
+    document.location.reload();
+  } else {
+    itemkey = prompt("        Enter Your Key");
+    
+    if (itemkey == null || itemkey == "") {
+      alert("Error (Item Key)❌ ");
+      document.location.reload();
+    }
+  }
+}
 appkey=appkey.trim();
 
 itemkey=itemkey.trim();
